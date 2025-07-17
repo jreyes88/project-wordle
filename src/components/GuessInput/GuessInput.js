7dd610c3386a44053f61c846940e9a2012ef4036
jjooeeyy <jreyes88@gmail.com>
2025-07-17 17:52:33 -0500

    fix: exercise 1

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,34 +2,28 @@ import React from "react";
 
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
   return (
-    <form
-      className="guess-input-wrapper"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (guess.length < 5) {
-          window.alert("Guess is too short!");
-          return;
-        } else if (guess.length > 5) {
-          window.alert("Guess is too long!");
-          return;
-        } else {
-          console.log(guess);
-          setGuess("");
-        }
-      }}
-    >
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
-        id="guess-input"
-        type="text"
-        value={guess}
+        required
         minLength="5"
         maxLength="5"
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={guess}
         onChange={(event) => {
-          event.preventDefault();
-          setGuess(event.target.value.toUpperCase());
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
         }}
+        id="guess-input"
+        type="text"
       />
     </form>
   );
